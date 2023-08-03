9e90fbe9f08c1654b5045175cef90d7ceda7f907
lincon_martins <lincon_martins@sicredi.com.br>
2023-08-03 06:52:57 -0300

    Create standard exception

--- src/main/java/com/lincon/restapi/services/AppUserService.java
+++ src/main/java/com/lincon/restapi/services/AppUserService.java
@@ -2,6 +2,7 @@ package com.lincon.restapi.services;
 
 import com.lincon.restapi.domain.AppUser;
 import com.lincon.restapi.repositories.AppUserRepository;
+import com.lincon.restapi.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,10 @@ public class AppUserService {
 
     @Autowired
     private AppUserRepository repository;
+
     public AppUser findById(Integer id) {
         Optional<AppUser> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Object not found ID:" + id + ", Type: " + AppUser.class.getName()));
     }
 }
